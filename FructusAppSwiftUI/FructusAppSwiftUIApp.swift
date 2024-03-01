bae7f6d83ed74b6b976ac358934f070d43628081
bilal-sagir <sagir.bilal@icloud.com>
2024-03-01 14:23:41 +0300

    Initial Commit

---
+++ FructusAppSwiftUI/FructusAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusAppSwiftUIApp.swift
+//  FructusAppSwiftUI
+//
+//  Created by Bilal on 1.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FructusAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
